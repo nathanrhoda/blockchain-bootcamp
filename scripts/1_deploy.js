ed5d643d4d74dc8fff8a7ea58b59838c5c7a4901
nathan.rhoda@gmail.com <mono@MSI.localdomain>
2024-01-03 13:15:32 +0200

    2. Smoke tests

---
+++ scripts/1_deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token");
+
+  // Deploy contract  
+  const token = await Token.deploy()  
+  await token.deployed()
+  console.log(`Token deployed to: ${token.address}\n`);
+
+}
+
+main()
+.then(() => process.exit(0))
+.catch(error => {
+  console.error(error);
+  process.exit(1);
+});

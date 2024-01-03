6ee0b0c77d58ada9afa57239ee0a213a33d3a545
Nathan Rhoda <nathan.rhoda@gmail.com>
2024-01-03 17:38:49 +0200

    Clean up and refactor tests

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,14 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "My Token";    
-    string public symbol = "MT";
+    string public name;
+    string public symbol;    
+    uint256 public totalSupply;
     uint256 public decimals = 18;
-    uint256 public totalSupply = 1000000 * (10 ** decimals); // 1, 000, 000 * 10^18;
+
+    constructor (string memory _name, string memory _symbol, uint256 _totalSupply) public {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10 ** decimals); // 1, 000, 000 * 10^18;
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -7,25 +7,30 @@ const tokens = (n) => {
 
 describe('Token', ()=> {
     let token    
+    let tokenName = 'My Token';
+    let symbol = 'MT';
+    let totalSupply = 1000000;
 
     beforeEach(async () => {
         const Token = await ethers.getContractFactory('Token');
-        token = await Token.deploy();
+        token = await Token.deploy(tokenName, symbol, totalSupply);
     });
 
-    it('has correct name', async () =>  {
-        expect(await token.name()).to.equal("My Token");                
-    });
-
-    it('has correct symbol', async () => {        
-        expect(await token.symbol()).to.equal("MT");                
-    });
-
-    it('has 18 decimals', async () => {        
-        expect(await token.decimals()).to.equal('18');                
-    });
-
-    it('has correct totalSupply', async () => {    
-        expect(await token.totalSupply()).to.equal(tokens('1000000'));                
+    describe('Deployment', () => {
+        it('has correct name', async () =>  {
+            expect(await token.name()).to.equal(tokenName);                
+        });
+    
+        it('has correct symbol', async () => {        
+            expect(await token.symbol()).to.equal(symbol);                
+        });
+    
+        it('has 18 decimals', async () => {        
+            expect(await token.decimals()).to.equal('18');                
+        });
+    
+        it('has correct totalSupply', async () => {    
+            expect(await token.totalSupply()).to.equal(tokens(totalSupply));                
+        });
     });
 });

898302a25bf0ad3e8f9cd9ba24a7af5c002b484d
Nathan Rhoda <nathan.rhoda@gmail.com>
2024-01-11 06:22:44 +0200

    Deposit tokens and relevant msg for insufficient balance

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./Token.sol";
 
 contract Exchange {
     address public feeAccount;
@@ -11,4 +12,8 @@ contract Exchange {
         feeAccount = _feeAccount;
         feePercent =_feePercent;
     }    
+
+    function depositToken(address _token, uint256 _amount) public {
+        Token(_token).transferFrom(msg.sender, address(this), _amount);
+    }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -6,17 +6,27 @@ const tokens = (n) => {
 }
 
 describe('Exchange', ()=> {
-    let deployer, feeAccount, exchange
+    let deployer, feeAccount, exchange, token1, user1, accounts
+    let tokenName = 'My Token';
+    let symbol = 'MT';
+    let totalSupply = '1000000';    
 
     const feePercent = 10;
 
     beforeEach(async () => {
+        const Exchange = await ethers.getContractFactory('Exchange');
+        const Token = await ethers.getContractFactory('Token');
+
+        token1 = await Token.deploy('Token1', 'TK1', totalSupply);
+
         accounts = await ethers.getSigners();
         deployer = accounts[0];    
         feeAccount = accounts[1];   
+        user1 = accounts[2];   
 
-        const Exchange = await ethers.getContractFactory('Exchange');
-        exchange = await Exchange.deploy(feeAccount.address, feePercent);
+        let transaction = await token1.connect(deployer).transfer(user1.address, tokens(100));
+        await transaction.wait();
+        exchange = await Exchange.deploy(feeAccount.address, feePercent);        
     });
 
     describe('Deployment', () => {
@@ -26,6 +36,27 @@ describe('Exchange', ()=> {
 
         it('tracks the fee percent', async () =>  {
             expect(await exchange.feePercent()).to.equal(feePercent);                
+        });
+    });
+    
+    describe('Depositing Tokens', () => {
+        let transaction, result
+
+        let amount = tokens(10)
+
+        beforeEach( async () => {
+            transaction = await token1.connect(user1).approve(exchange.address, amount);
+            transaction = await exchange.connect(user1).depositToken(token1.address, amount);
+            result = await transaction.wait();
+        });
+
+        describe('Success', () => {
+            it('tracks the token deposit', async () => {
+                expect(await token1.balanceOf(exchange.address)).to.equal(amount);
+            });
+        });
+        
+        describe('Failure', () => {
         });
     });
 });

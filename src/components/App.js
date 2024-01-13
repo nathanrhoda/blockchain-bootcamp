12da8c6f6b1605d0bdf800b26ac37a160231e10b
Nathan Rhoda <nathan.rhoda@gmail.com>
2024-01-13 19:36:30 +0200

    Get structure in place for components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world</h1>
+      <p>This is placeholder text</p>
+    </div>
+  );
+}
+
+export default App;

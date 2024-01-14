3d77d3e9072dae41b9b9c6f64b3a948853c44735
Nathan Rhoda <nathan.rhoda@gmail.com>
2024-01-14 10:09:57 +0200

    hooked ui up to smart contract

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
+import { useEffect } from 'react';
+import { ethers } from 'ethers';
+import WZAR_ABI from '../abis/Token.json';
+import config from '../config.json';
 import '../App.css';
 
 function App() {
 
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+    console.log(accounts[0])
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const { chainId } = await provider.getNetwork()
+
+    const wzar = new ethers.Contract( config[chainId].WZAR.address, WZAR_ABI, provider)
+    console.log(wzar.address)
+
+    const symbol = await wzar.symbol();
+    console.log(symbol)
+
+    // const exchange = new ethers.Contract( config[chainId].Exchange.address, Exchange_ABI, provider)
+
+  }
+
+  useEffect(() => { 
+    loadBlockchainData()
+  })
   return (
     <div>
 

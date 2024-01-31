1a2adf5734520925f257e5bb25d51d2cdb5c7f8f
Nathan Rhoda <nathan.rhoda@gmail.com>
2024-01-31 19:00:52 +0200

    Trades

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ import Balance from './Balance';
 import Order from './Order';
 import OrderBook from './OrderBook';
 import PriceChart from './PriceChart';
-
+import Trades from './Trades';
 
 function App() {
   const dispatch = useDispatch();
@@ -75,7 +75,7 @@ function App() {
 
           {/* Transactions */}
 
-          {/* Trades */}
+          <Trades />
 
           <OrderBook />
 

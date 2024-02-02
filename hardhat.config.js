525ab7807bd767b703800e75b798470da50c1081
Nathan Rhoda <nathan.rhoda@gmail.com>
2024-02-02 22:26:47 +0200

    Deployment to sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
@@ -7,7 +8,11 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {}
+    localhost: {},
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(",")
+    }
   },
   paths: {
     tests: "./test",
